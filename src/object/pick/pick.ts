0e206e8aa3320510d6869a05210843ff4fd5d66d
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-09-01 21:03:20 +0200

    feat(typescript): modify types to support typescript 4.8 (#9)

--- src/object/pick/pick.ts
+++ src/object/pick/pick.ts
@@ -1,12 +1,17 @@
 import type { SimplifyOnce } from '../../type/simplify'
 
-export function pickBy<T, Predicate extends ([key, value]: [key: keyof T, value: T[keyof T]]) => boolean>(
-    obj: T,
-    predicate: Predicate
-): Partial<T> {
-    return Object.fromEntries(Object.entries(obj).filter(([k, v]) => predicate([k as keyof T, v]))) as unknown as Partial<T>
+export function pickBy<
+    T extends ArrayLike<unknown> | Record<string, unknown>,
+    Predicate extends ([key, value]: [key: keyof T, value: T[keyof T]]) => boolean
+>(obj: T, predicate: Predicate): Partial<T> {
+    return Object.fromEntries(
+        Object.entries(obj).filter(([k, v]) => predicate([k as keyof T, v as T[keyof T]]))
+    ) as unknown as Partial<T>
 }
 
-export function pick<T, K extends keyof T>(obj: T, keys: readonly K[]): SimplifyOnce<Pick<T, K>> {
+export function pick<T extends ArrayLike<unknown> | Record<string, unknown>, K extends keyof T>(
+    obj: T,
+    keys: readonly K[]
+): SimplifyOnce<Pick<T, K>> {
     return Object.fromEntries(Object.entries(obj).filter(([k]) => keys.includes(k as K))) as unknown as SimplifyOnce<Pick<T, K>>
 }
