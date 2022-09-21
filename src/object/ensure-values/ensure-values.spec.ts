931f162c4d33afee254b809a9fd925c0a3fef965
Mick van Duijn <m.vanduijn@zefiros.io>
2022-09-21 14:27:14 +0200

    feat: implement ensure-values

---
+++ src/object/ensure-values/ensure-values.spec.ts
@@ -0,0 +1,22 @@
+import { ensureValues } from './ensure-values'
+
+import { constant, dict, forAll, oneOf, unknown } from '../../random'
+
+test.only('ensure-values', () => {
+    forAll(dict(oneOf(constant(undefined), unknown({ undefined: false }))), (x) => {
+        const config = ensureValues(x)
+        for (const k of [...Object.keys(x), ...Object.getOwnPropertySymbols(x)] as Array<keyof typeof x>) {
+            if (x[k] === undefined) {
+                try {
+                    void config[k]
+                    return false
+                } catch (err) {
+                    //
+                }
+            } else if (config[k] !== x[k]) {
+                return false
+            }
+        }
+        return true
+    })
+})
