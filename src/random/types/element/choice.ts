9ece1e73447d12608476347a16c907fbd3c252be
Paul Visscher <p.e.visscher@skyleague.io>
2022-09-30 11:36:55 +0200

    fix(element): correctly infer element type for string arrays

--- src/random/types/element/choice.ts
+++ src/random/types/element/choice.ts
@@ -3,7 +3,9 @@ import type { Dependent } from '../../arbitrary/dependent'
 import { makeDependent } from '../../arbitrary/dependent'
 import { integer } from '../integer'
 
-export function element<T>(elements: T extends string ? string : T[]): Dependent<T extends string ? string : T> {
+export function element<T extends unknown[] | string>(
+    elements: T extends string ? string : T
+): Dependent<T extends string ? string : T[number]> {
     const aint = integer({ min: 0, max: elements.length })
     return makeDependent((context) => mapTree(aint.value(context), (n) => elements[n] as T extends string ? string : T))
 }
