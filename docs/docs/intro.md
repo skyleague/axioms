6233552ef3cb1e3a7cbe5fac6e94542a6bf2e9d2
Paul Visscher <p.e.visscher@skyleague.io>
2022-10-20 00:27:48 +0200

    docs: add docusaur documentation

---
+++ docs/docs/intro.md
@@ -0,0 +1,15 @@
+---
+sidebar_position: 0
+title: Overview
+slug: /
+---
+
+# @skyleague/axioms
+
+> The art of doing mathematics consists in finding that special case which contains all the germs of generality.
+>
+> -   David Hilbert
+
+Axioms is a library of *small* composable functions, providing functional programming functionality to regular typescript code.
+
+Writing typesafe code shouldn't be hard. However, writing generic functions to do so *is*. Axioms do the hard work for you. The library is entirely tree shakeable and small as we can make it.
---
+++ docs/docusaurus.config.js
@@ -0,0 +1,101 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const package = require('../package.json')
+const [organizationName, projectName] = package.name.replace('@', '').split('/')
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: package.name,
+  tagline: package.description,
+  url: package.homepage,
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  organizationName,
+  projectName,
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  plugins: [
+    [
+      'docusaurus-plugin-typedoc-api',
+      {
+        projectRoot: require('path').join(__dirname, '..'),
+        packages: ['.']
+      },
+    ],
+  ],
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // algolia: {
+      //   appId: 'YOUR_APP_ID',
+      //   apiKey: 'YOUR_SEARCH_API_KEY',
+      //   indexName: 'YOUR_INDEX_NAME',
+      //   contextualSearch: true,
+      // },
+      navbar: {
+        title: package.name,
+        logo: {
+          alt: 'SkyLeague Logo',
+          src: 'img/skyleague.png',
+          srcDark: 'img/skyleaguew.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            to: 'api',
+            label: 'API',
+            position: 'left',
+          },
+          {
+            href: package.repository.url,
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} SkyLeague Technologies B.V.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
