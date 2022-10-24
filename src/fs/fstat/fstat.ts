c56dd3a85e1ef327ae37d774e091aadb736d4b17
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-10-24 14:42:21 +0200

    docs(fs): add docs to the fs module (#28)

--- src/fs/fstat/fstat.ts
+++ src/fs/fstat/fstat.ts
@@ -6,6 +6,25 @@ import { Nothing } from '../../type/maybe'
 import type { Stats } from 'fs'
 import { promises } from 'fs'
 
+/**
+ * Return the fstat results with improved edge case handling.
+ *
+ * ### Example
+ * ```ts
+ * await fstat("./non-existing")
+ * // => { right: Nothing }
+ *
+ * await fstat("./existing")
+ * // => { right: Stats }
+ * ```
+ *
+ *
+ * @param path - The file path.
+ * @returns The fstat response.
+ *
+ * @experimental
+ * @category Filesystem
+ */
 export async function fstat(file: string): Promise<Either<unknown, Maybe<Stats>>> {
     try {
         return { right: await promises.stat(file) }
