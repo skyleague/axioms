4ff1101f979e005b9018c921b73aaf6c318a26b7
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-10-24 14:33:25 +0200

    Update src/fs/fstat/fstat.ts
    Co-authored-by: MickVanDuijn <m.vanduijn@zefiros.io>
    Signed-off-by: pevisscher <2388946+pevisscher@users.noreply.github.com>

--- src/fs/fstat/fstat.ts
+++ src/fs/fstat/fstat.ts
@@ -20,9 +20,6 @@ import { promises } from 'fs'
  *
  *
  * @param path - The path to normalize
- * @param options - Configure how the normalization should happen.
- * @param options.forceForwardSlash - Set to true if we need to replace all backwards slashes to forward. By default
- *                                    we check the separator used on the current OS, and only replace when needed.
  * @returns The fstat response.
  *
  * @experimental
