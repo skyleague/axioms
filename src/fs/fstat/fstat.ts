1f4c10a47528b880ae30265118f2c3d74dade101
Paul Visscher <p.e.visscher@skyleague.io>
2022-10-24 09:41:42 +0200

    docs(fs): add docs to the fs module

--- src/fs/fstat/fstat.ts
+++ src/fs/fstat/fstat.ts
@@ -6,6 +6,28 @@ import { Nothing } from '../../type/maybe'
 import type { Stats } from 'fs'
 import { promises } from 'fs'
 
+/**
+ * Return the fstat results with improved edge case handling.
+ *
+ * ### Example
+ * ```ts
+ * await fstat("./non-existing")
+ * // => { right: Nothing }
+ *
+ * await fstat("./existing")
+ * // => { right: Stats }
+ * ```
+ *
+ *
+ * @param path - The path to normalize
+ * @param options - Configure how the normalization should happen.
+ * @param options.forceForwardSlash - Set to true if we need to replace all backwards slashes to forward. By default
+ *                                    we check the separator used on the current OS, and only replace when needed.
+ * @returns The normalized path.
+ *
+ * @experimental
+ * @category Filesystem
+ */
 export async function fstat(file: string): Promise<Either<unknown, Maybe<Stats>>> {
     try {
         return { right: await promises.stat(file) }
--- src/fs/normalize-path/normalize-path.ts
+++ src/fs/normalize-path/normalize-path.ts
@@ -1,11 +1,31 @@
 import { sep } from 'path'
 
-/// or just backwards? ;)
 const isBackwardSlash = sep === '\\'
 
-export function normalizePath(
-    path: string,
-    { forceForwardSlash = isBackwardSlash }: { forceForwardSlash?: boolean } = {}
-): string {
+/**
+ * Normalize the given path to always use forward slashes, no matter the underlying OS.
+ *
+ * ### Example
+ * ```ts
+ * // On windows
+ * normalizePath("foo\\bar")
+ * // => "foo/bar"
+ *
+ * // On unix
+ * normalizePath("foo/bar")
+ * // => "foo/bar"
+ * ```
+ *
+ *
+ * @param path - The path to normalize
+ * @param options - Configure how the normalization should happen.
+ * @param options.forceForwardSlash - Set to true if we need to replace all backwards slashes to forward. By default
+ *                                    we check the separator used on the current OS, and only replace when needed.
+ * @returns The normalized path.
+ *
+ * @category Filesystem
+ */
+export function normalizePath(path: string, options: { forceForwardSlash?: boolean } = {}): string {
+    const { forceForwardSlash = isBackwardSlash } = options
     return forceForwardSlash ? path.replace(/\\/g, '/') : path
 }
