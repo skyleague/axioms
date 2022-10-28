c25689b708156ce9d21a54edbf5fe57819264fea
Paul Visscher <p.e.visscher@skyleague.io>
2022-10-28 14:21:18 +0200

    docs: documentation for at, equal, fold, filter, and group-by

--- src/iterator/at/at.ts
+++ src/iterator/at/at.ts
@@ -6,6 +6,27 @@ import { drop } from '../drop'
 
 export type Nth<N extends number, T> = T extends readonly any[] ? T[N] : never
 
+/**
+ * `at` returns the nth element of a traversable, or {@link Nothing} if the traversable is too short
+ *
+ * ### Example
+ * ```ts
+ * at([1, 2, 3], 1)
+ * // => 2
+ *
+ * at([1, 2, 3], 4)
+ * // => Nothing
+ * ```
+ *
+ * @param xs - The traversable to find the element in.
+ * @param n - The index of the element to retrieve.
+ * @returns If the traversable contains an element as position `n` we return that element, otherwise {@link NOthing}.
+ *
+ * @typeParam T - The element type.
+ * @typeParam N - The index of the element.
+ *
+ * @group Iterators
+ */
 export function at<Xs extends any[], N extends number>(
     xs: readonly [...Xs],
     n: N
@@ -19,18 +40,93 @@ export function at<T, N extends number = number>(xs: Traversable<T>, n: N): Mayb
     return head(drop(xs, n))
 }
 
+/**
+ * Return the first element of a traversable, or nothing if the traversable is empty.
+ *
+ * The `first` function is a good example of how to use the at function. It's also a good example of how
+ * to use the {@link Maybe} type.
+ *
+ * ### Example
+ * ```ts
+ * first([1, 2, 3])
+ * // => 1
+ *
+ * first([])
+ * // => Nothing
+ * ```
+ *
+ * @param xs - The traversable to find the element in.
+ *
+ * @returns The first element of the traversable, otherwise {@link Nothing}.
+ *
+ * @typeParam T - The element type.
+ *
+ * @see {@link at}
+ *
+ * @group Iterators
+ */
 export function first<T>(xs: T): T extends readonly [infer N0, ...unknown[]] ? N0 : Nothing
 export function first<T>(xs: Traversable<T>): Maybe<T>
 export function first<T>(xs: Traversable<T>): Maybe<T> {
     return at(xs, 0)
 }
 
+/**
+ * Return the second element of a traversable, or nothing if the traversable is empty.
+ *
+ * The `second` function is a good example of how to use the at function. It's also a good example of how
+ * to use the {@link Maybe} type.
+ *
+ * ### Example
+ * ```ts
+ * second([1, 2, 3])
+ * // => 2
+ *
+ * second([])
+ * // => Nothing
+ * ```
+ *
+ * @param xs - The traversable to find the element in.
+ *
+ * @returns The second element of the traversable, otherwise {@link Nothing}.
+ *
+ * @typeParam T - The element type.
+ *
+ * @see {@link at}
+ *
+ * @group Iterators
+ */
 export function second<T>(xs: readonly [unknown, T, ...unknown[]]): T
 export function second<T>(xs: Traversable<T>): Maybe<T>
 export function second<T>(xs: Traversable<T>): Maybe<T> {
     return at(xs, 1)
 }
 
+/**
+ * Return the third element of a traversable, or nothing if the traversable is empty.
+ *
+ * The `third` function is a good example of how to use the at function. It's also a good example of how
+ * to use the {@link Maybe} type.
+ *
+ * ### Example
+ * ```ts
+ * third([1, 2, 3])
+ * // => 2
+ *
+ * third([])
+ * // => Nothing
+ * ```
+ *
+ * @param xs - The traversable to find the element in.
+ *
+ * @returns The third element of the traversable, otherwise {@link Nothing}.
+ *
+ * @typeParam T - The element type.
+ *
+ * @see {@link at}
+ *
+ * @group Iterators
+ */
 export function third<T>(xs: readonly [unknown, unknown, T, ...unknown[]]): T
 export function third<T>(xs: Traversable<T>): Maybe<T>
 export function third<T>(xs: Traversable<T>): Maybe<T> {
--- src/iterator/fold/fold.ts
+++ src/iterator/fold/fold.ts
@@ -5,6 +5,34 @@ import { toTraversable } from '../../type/traversable'
 import type { Traversable } from '../../type/traversable'
 import { uncons } from '../uncons'
 
+/**
+ * Takes a collection of values, a reducer function, and an initial value, and return the result of
+ * applying the reducer function to each value in the collection, starting with the initial value
+ *
+ * ### Example
+ * ```ts
+ * foldl([1, 2], (a, b) => a + b)
+ * // => 3
+ * ```
+ *
+ * ### Alternatives
+ * - [ECMAScript - Array.reduce](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce)
+ * - [Lodash - reduce](https://lodash.com/docs/4.17.15#reduce)
+ *
+ * @param xs - The elements to fold.
+ * @param reducer - A function that takes the accumulator and the current value and returns the new accumulator.
+ * @param {R} init - The initial value of the accumulator.
+ *
+ * @returns The last value of the accumulator.
+ *
+ *
+ * @returns This function returns true if all elements in the iterators are equal, and false otherwise.
+ *
+ * @typeParam T - The element type.
+ * @typeParam R - The accumulation type.
+ *
+ * @group Iterators
+ */
 export function foldl<T, R = T>(xs: Traversable<T>, reducer: (acc: R, val: T) => R, init: R): R {
     let acc = init
     for (const x of xs) {
@@ -13,6 +41,9 @@ export function foldl<T, R = T>(xs: Traversable<T>, reducer: (acc: R, val: T) =>
     return acc
 }
 
+/**
+ * @experimental
+ */
 export function foldl1<T>(xs: Traversable<T>, reducer: (acc: T, val: T) => T): Maybe<T> {
     const [head, rest] = uncons(xs)
     // if first is undefined due to the length of the Iterable
@@ -22,5 +53,13 @@ export function foldl1<T>(xs: Traversable<T>, reducer: (acc: T, val: T) => T): M
     }
     return Nothing
 }
+
+/**
+ * @inheritDoc foldl
+ *
+ * @see {@link foldl}
+ *
+ * @group Iterators
+ */
 
 export const reduce = foldl
