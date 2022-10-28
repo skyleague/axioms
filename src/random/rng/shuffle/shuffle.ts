04c41e2cd574cc540d44e6ed6c927daae9ae2bed
Paul Visscher <p.e.visscher@skyleague.io>
2022-10-28 14:20:36 +0200

    refactor(shuffle): let shuffle work on traversable iterators

--- src/random/rng/shuffle/shuffle.ts
+++ src/random/rng/shuffle/shuffle.ts
@@ -1,6 +1,9 @@
-export function shuffle<T>(xs: readonly T[], random: () => number = Math.random): T[] {
-    const axs = [...xs]
-    for (let i = xs.length - 1; i > 0; i--) {
+import { collect } from '../../../array'
+import type { Traversable } from '../../../type'
+
+export function shuffle<T>(xs: Traversable<T>, random: () => number = Math.random): T[] {
+    const axs = collect(xs)
+    for (let i = axs.length - 1; i > 0; i--) {
         const j = Math.floor(random() * (i + 1))
         const temp = axs[i]
         axs[i] = axs[j]
