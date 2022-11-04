9aecb6df5fd73438052c1fec3dab9f5071bd47b1
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-11-04 18:41:21 +0100

    docs(functions): add documentations to the functions group (#32)
    * feat(arbitrary): add monad interface for arbitraries
    
    * refactor(shuffle): let shuffle work on traversable iterators
    
    * docs(combinators): write documentation for either combinators
    
    * docs(fs): correctly set filesystem group
    
    * docs(functions): add documentations to the functions group

--- src/function/tuple/tuple.ts
+++ src/function/tuple/tuple.ts
@@ -1,6 +1,9 @@
 import type { CurriedVariadic } from '../curry'
 import { curryVariadic } from '../curry'
 
+/**
+ * @experimental
+ */
 export function curryTuple<T extends unknown[], L extends number>(n: L): CurriedVariadic<[...T], [...T], L> {
     return curryVariadic((...xs: [...T]): [...T] => xs, n)
 }
