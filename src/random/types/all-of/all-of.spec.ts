e78b7e29d09c2cd46c3c86342182d2dab0c72c5f
Mick van Duijn <m.vanduijn@zefiros.io>
2022-11-04 15:24:39 +0100

    test(all-of): add test for ordering

--- src/random/types/all-of/all-of.spec.ts
+++ src/random/types/all-of/all-of.spec.ts
@@ -1,9 +1,11 @@
 import { allOf } from './all-of'
 
 import { isString } from '../../../guard'
-import { forAll } from '../../arbitrary'
+import { chainArbitrary, forAll } from '../../arbitrary'
+import { constant } from '../helper'
 import { object } from '../object'
 import { string } from '../string'
+import { tuple } from '../tuple'
 
 test('simple', () => {
     const aFoo = object({ foo: string() })
@@ -11,4 +13,18 @@ test('simple', () => {
     forAll(allOf(aFoo, aBar), (x) => {
         return isString(x.foo) && isString(x.bar)
     })
+})
+
+test('merge order', () => {
+    const aFoo = object({ foo: string(), bar: string() })
+    const aBar = object({ bar: string() })
+
+    forAll(
+        chainArbitrary(tuple(aFoo, aBar), ([foo, bar]) => {
+            return tuple(constant(foo), constant(bar), allOf(constant(foo), constant(bar)))
+        }),
+        ([foo, bar, x]) => {
+            expect(x).toEqual({ ...foo, ...bar })
+        }
+    )
 })
