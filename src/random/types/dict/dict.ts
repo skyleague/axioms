1d11fa568fbf69acf53b4dc3ac10ac398910ae39
Paul Visscher <p.e.visscher@skyleague.io>
2022-11-28 15:59:16 +0100

    refactor(arbitrary): do not type narrow dependent to arbitarry

--- src/random/types/dict/dict.ts
+++ src/random/types/dict/dict.ts
@@ -2,9 +2,10 @@ import { mapTree } from '../../../algorithm/tree'
 import { isArray } from '../../../guard/is-array'
 import type { RelaxedPartial } from '../../../type/partial'
 import type { Arbitrary } from '../../arbitrary/arbitrary'
+import type { Dependent } from '../../arbitrary/dependent'
 import { dependentArbitrary } from '../../arbitrary/dependent'
-import { propertyKey } from '../property-key'
 import { set } from '../set'
+import { string } from '../string'
 import { tuple } from '../tuple'
 
 export interface DictGenerator<T> {
@@ -16,9 +17,9 @@ export interface DictGenerator<T> {
 export function dict<T, K extends PropertyKey>(
     keyValue: Arbitrary<T> | [Arbitrary<K>, Arbitrary<T>],
     context: RelaxedPartial<DictGenerator<T>> = {}
-): Arbitrary<Record<string, T>> {
+): Dependent<Record<string, T>> {
     const { minLength = 0, maxLength = 10 } = context
-    const [key, value] = isArray(keyValue) ? keyValue : [propertyKey(), keyValue]
+    const [key, value] = isArray(keyValue) ? keyValue : [string(), keyValue]
     const aset = set(tuple(key, value), { eq: (a, b) => a[0] === b[0], minLength, maxLength })
 
     return dependentArbitrary((ctx) => mapTree(aset.value(ctx), (kvs) => Object.fromEntries(kvs)))
