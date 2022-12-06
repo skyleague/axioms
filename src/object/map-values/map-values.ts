6bc6267b625be56ac47b052fd2321a134a105f71
Paul Visscher <p.e.visscher@skyleague.io>
2022-12-06 13:36:40 +0100

    docs(object): add documentation on object functions

--- src/object/map-values/map-values.ts
+++ src/object/map-values/map-values.ts
@@ -1,6 +1,8 @@
 import { entriesOf } from '../entries-of'
 import { fromEntries } from '../from-entries'
 
+ */ 
+export function mapValues<T extends ArrayLike<unknown> | {}, Mapper extends (v: T[keyof T], k: keyof T) => unknown>(
 /**
  * Returns an object with same keys, where the original values are converted using a mapper.
  *
