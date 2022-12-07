97f357d4cbf80d7f42f473cb6437aa3b8474efdf
pevisscher <2388946+pevisscher@users.noreply.github.com>
2022-12-07 18:55:26 +0100

    chore: Update src/object/map-values/map-values.ts
    Co-authored-by: MickVanDuijn <m.vanduijn@zefiros.io>
    Signed-off-by: pevisscher <2388946+pevisscher@users.noreply.github.com>

--- src/object/map-values/map-values.ts
+++ src/object/map-values/map-values.ts
@@ -2,25 +2,23 @@ import { entriesOf } from '../entries-of'
 import { fromEntries } from '../from-entries'
 
 /**
- * Returns the names of the enumerable string properties and methods of an object.
+ * Returns an object with same keys, where the original values are converted using a mapper.
  *
  * ### Example
  * ```ts
- * keysOf([["foo", "bar"]])
- * // => ["foo"]
- *
- * keysOf({})
- * // => []
+ * mapValues({ foo: 0, bar: 2 }, (x) => x + 1)
+ * // => { foo: 1, bar: 3 }
  * ```
  *
  * ### Alternatives
- * - [Lodash - keys](https://lodash.com/docs/4.17.15#keys)
+ * - [Lodash - mapValues](https://lodash.com/docs/4.17.15#mapValues)
  *
- * @param obj - The object to get the keys from.
+ * @param obj - The object to map the values from.
  * @returns The keys as an array.
  *
  * @group Object
- */ export function mapValues<T extends ArrayLike<unknown> | {}, Mapper extends (v: T[keyof T], k: keyof T) => unknown>(
+ */
+export function mapValues<T extends ArrayLike<unknown> | {}, Mapper extends (v: T[keyof T], k: keyof T) => unknown>(
     obj: T,
     mapper: Mapper
 ): {
