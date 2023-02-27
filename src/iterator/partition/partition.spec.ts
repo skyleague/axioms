4720f89426d319e22fefb7eb5f7262e6f896ffa2
MickVanDuijn <m.vanduijn@zefiros.io>
2023-02-27 11:08:40 +0100

    feat(partition): implement partition function (#74)

---
+++ src/iterator/partition/partition.spec.ts
@@ -0,0 +1,65 @@
+import { partition } from './partition'
+
+import type { Mappable } from '../..'
+import {
+    array,
+    boolean,
+    float,
+    forAll,
+    integer,
+    isBoolean,
+    isNumber,
+    isString,
+    mappableFunc,
+    oneOf,
+    string,
+    toTraversable,
+    tuple,
+    xoroshiro128plus,
+} from '../..'
+
+function* interleave<X, Y>(xs: X[], ys: Y[], seed: number) {
+    const rng = xoroshiro128plus(BigInt(seed))
+
+    for (let iX = 0, iY = 0; iX < xs.length || iY < ys.length; ) {
+        if ((rng.sample() < 0.5 && iX < xs.length) || iY >= ys.length) {
+            yield xs[iX++]
+        } else {
+            yield ys[iY++]
+        }
+    }
+}
+
+test('simple', () => {
+    const xs = ['a', 0, 'b', 'c', 1, 'd', 2, 3]
+    expect(partition(xs, isString)).toEqual([
+        ['a', 'b', 'c', 'd'],
+        [0, 1, 2, 3],
+    ])
+})
+
+test('for [xs, ys] = partition zs, |zs| == |xs| + |ys|', () => {
+    forAll(
+        tuple(mappableFunc(), array(oneOf(float(), string(), boolean()))),
+        ([f, zs]: [<T>(us: T[]) => Mappable<T>, (boolean | number | string)[]]) => {
+            for (const fn of [isString, isBoolean, isNumber]) {
+                const [xs, ys] = partition(toTraversable(f(zs)), fn as <T>(x: unknown) => x is T)
+                expect(zs.length).toEqual(xs.length + ys.length)
+            }
+        }
+    )
+})
+
+test('partition (interleave xs, ys), isX == [xs, ys]', () => {
+    forAll(
+        tuple(mappableFunc(), array(string()), array(float()), integer()),
+        ([f, xs, ys, seed]: [<T>(us: T[]) => Mappable<T>, string[], number[], number]) => {
+            const zs = [...interleave(xs, ys, seed)]
+
+            expect(zs.length).toEqual(xs.length + ys.length)
+            const [Xs, Ys] = partition(toTraversable(f(zs)), isString)
+            expect(Xs).toEqual(xs)
+            expect(Ys).toEqual(ys)
+        }
+    )
+})
