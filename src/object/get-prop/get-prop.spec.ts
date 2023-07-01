7b8ef38654a109278a2adca8266144de5d82cf8f
pevisscher <2388946+pevisscher@users.noreply.github.com>
2023-07-01 16:20:22 +0200

    feat(get-prop): add dot path getter (#87)

---
+++ src/object/get-prop/get-prop.spec.ts
@@ -0,0 +1,48 @@
+import { getProp } from './get-prop.js'
+
+import { Nothing, dict, enumerate, forAll, isObject, string, tuple, unknown } from '../../index.js'
+
+import { it, expect, assertType } from 'vitest'
+
+it('retrieves simple paths', () => {
+    const obj = {
+        a: {
+            b: {
+                c: 5,
+            },
+        },
+    }
+    const c = getProp(obj, 'a.b.c')
+    expect(c).toEqual(5)
+    assertType<number>(c)
+
+    const d = getProp(obj, 'a.b.d')
+    expect(d).toEqual(Nothing)
+})
+
+it('returns undefined for non-existent properties', () => {
+    forAll(tuple(unknown(), string()), ([obj, path]) => {
+        const [first] = path.split('.')
+        if (isObject(obj) && first !== undefined) {
+            // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+            delete obj[first]
+        }
+        expect(getProp(obj, path)).toEqual(Nothing)
+    })
+})
+
+it('returns the correct value for existing properties', () => {
+    forAll(tuple(dict(unknown()), string(), unknown()), ([obj, path, value]) => {
+        const pathArr = path.split('.')
+        let current = obj as any
+        for (const [i, key] of enumerate(pathArr)) {
+            if (i === pathArr.length - 1) {
+                current[key] = value
+            } else {
+                current[key] = {}
+            }
+            current = current[key]
+        }
+        expect(getProp(obj, path)).toEqual(value)
+    })
+})
