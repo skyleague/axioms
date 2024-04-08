011ff8941f687f97ddc6103d068fb04bfb27541a
Paul Visscher <p.e.visscher@skyleague.io>
2024-04-08 14:15:40 +0200

    refactor(evaluate): more consistent type inference

--- src/function/evaluate/evaluate.spec.ts
+++ src/function/evaluate/evaluate.spec.ts
@@ -3,7 +3,7 @@ import { evaluate } from './evaluate.js'
 import { sleep } from '../../async/index.js'
 import { asyncForAll, forAll, unknown } from '../../random/index.js'
 
-import { it } from 'vitest'
+import { it, expectTypeOf } from 'vitest'
 
 it('constant', () => {
     forAll(unknown(), (x) => evaluate(x) === x)
@@ -22,4 +22,21 @@ it('async fn', async () => {
             })) === x
         )
     })
+})
+
+it('types', () => {
+    expectTypeOf(evaluate(1)).toEqualTypeOf<1>()
+    expectTypeOf(evaluate('foo')).toEqualTypeOf<'foo'>()
+    expectTypeOf(evaluate(true)).toEqualTypeOf<true>()
+    expectTypeOf(evaluate(null)).toEqualTypeOf<null>()
+
+    expectTypeOf(evaluate(() => 1)).toEqualTypeOf<number>()
+    expectTypeOf(evaluate(() => 'foo')).toEqualTypeOf<string>()
+    expectTypeOf(evaluate(() => true)).toEqualTypeOf<boolean>()
+    expectTypeOf(evaluate(() => null)).toEqualTypeOf<null>()
+
+    expectTypeOf(evaluate((..._: any[]) => 1)).toEqualTypeOf<number>()
+    expectTypeOf(evaluate((..._: any[]) => 'foo')).toEqualTypeOf<string>()
+    expectTypeOf(evaluate((..._: any[]) => true)).toEqualTypeOf<boolean>()
+    expectTypeOf(evaluate((..._: any[]) => null)).toEqualTypeOf<null>()
 })
